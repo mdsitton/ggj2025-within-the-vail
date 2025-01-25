efaf368235fd4fb862fd8126068d1380d4a92998
Staik <oStaiko45@gmail.com>
2025-01-24 20:04:02 -0600

    Initial Commit - Unity XR Interaction Toolkit

---
+++ Assets/_Scripts/Gun.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject hitEnemyVFX;
+    public GameObject hitWallVFX;
+    public GameObject muzzleFlashVFX;
+    public AudioSource shootSFX;
+
+    public int maxAmmo;
+
+    void Start()
+    {
+        
+    }
+
+    public void TriggerDown ()
+    {
+        
+    }
+
+    public void TriggerUp()
+    {
+      
+    }
+}
