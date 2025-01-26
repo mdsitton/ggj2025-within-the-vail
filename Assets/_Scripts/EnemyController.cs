8b1b20af8f6d5dc3ef7de3b851fa5b49b19618fa
Matthew sitton <matthewsitton@gmail.com>
2025-01-26 15:04:34 -0600

    Enemy

--- Assets/_Scripts/EnemyController.cs
+++ Assets/_Scripts/EnemyController.cs
@@ -8,27 +8,27 @@ public class EnemyController : MonoBehaviour, IAiStateMachine, ITarget
     [Range(0.01f, 2.0f)]
     [Tooltip("Scales the time between ai state checks")]
     public float reactionSpeed = 1.0f;
-    
+
     [Range(0.0f, 1000.0f)]
     public float visionRange = 10.0f;
-    
+
     [Range(0.0f, 1000.0f)]
     public float attackRange = 1.0f;
-    
+
     [Range(1, 1000)]
     public int attackDamage = 10;
 
     [Range(0.0f, 20.0f)]
     [Tooltip("The time between attacks")]
     public float attackCooldown = 1.0f;
-    
+
     [Range(0, 10000)]
     public int health = 100;
-    
+
     private int playerMask;
 
     private NavMeshAgent agent;
-    
+
     private ITarget currentTarget;
 
     private void Awake()
@@ -36,16 +36,15 @@ public class EnemyController : MonoBehaviour, IAiStateMachine, ITarget
         playerMask = LayerMask.GetMask("Player");
         agent = GetComponent<NavMeshAgent>();
     }
-    
+
     // IAiStateMachine Implementation
-    
+
     private static Collider[] sphereResults = new Collider[64];
-    
+
     public AiStateController AiController { get; set; }
-    
+
     private ITarget FindTarget(float radius)
     {
-        // TODO - we need a player collider
         var count = Physics.OverlapSphereNonAlloc(transform.position, radius, sphereResults, playerMask);
         if (count == 0)
         {
@@ -60,22 +59,22 @@ public class EnemyController : MonoBehaviour, IAiStateMachine, ITarget
                 continue;
             }
 
-            // Check that we have line of sight to the player
-            Physics.Raycast(transform.position, player.transform.position - transform.position, out var hit, radius, playerMask);
-                
-            if (hit.collider != player)
-            {
-                Debug.Log($"Player is not in line of sight {hit.collider}", gameObject);
-                return null;
-            }
-            
+            // // Check that we have line of sight to the player
+            // Physics.Raycast(transform.position, player.transform.position - transform.position, out var hit, radius, playerMask);
+
+            // if (hit.collider != player)
+            // {
+            //     Debug.Log($"Player is not in line of sight {hit.collider}", gameObject);
+            //     return null;
+            // }
+
             Debug.Log("Found player in radius, attacking", gameObject);
             return target;
         }
         Debug.Log("Player not found in radius", gameObject);
         return null;
     }
-    
+
     public AiState OnIdleState(AiState previousState)
     {
         return AiPhase.Patrol.AsState(0.25f / reactionSpeed);
@@ -134,7 +133,7 @@ public class EnemyController : MonoBehaviour, IAiStateMachine, ITarget
         onKill.Invoke();
         return AiPhase.Dead.AsState(10.0f);
     }
-    
+
     // ITarget Implementation
     TargetType ITarget.TargetType => TargetType.Enemy;
     Vector3 ITarget.GetPosition() => transform.position;
@@ -145,11 +144,11 @@ public class EnemyController : MonoBehaviour, IAiStateMachine, ITarget
         {
             currentTarget = attacker;
         }
-        
+
         if (damage > health)
         {
             health = 0;
-            
+
             return;
         }
         health -= damage;
